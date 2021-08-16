2856f99924bed8f6d85b64d9befc12a9fc555fee
zhoujian2 <zhoujian2@sensetime.com>
2021-08-16 17:46:15 +0800

    improve pose_recognition_from_camera.py

--- pose_recognition_from_camera.py
+++ pose_recognition_from_camera.py
@@ -5,95 +5,92 @@ import math
 import mediapipe as mp
 import numpy as np
 import sys
+import time
 
 
-def min_enclosing_face_circle(landmark_info):
-    point_set = np.empty((0, 2), int)
-
-    index_list = [2, 5, 7, 8, 9, 10]
-    for index in index_list:
-        np_landmark_point = [
-            np.array((landmark_info[index][2][0], landmark_info[index][2][1]))
-        ]
-        point_set = np.append(point_set, np_landmark_point, axis=0)
-
-    return cv.minEnclosingCircle(points=point_set)
-
-
-def draw(image, landmarks, color=(255, 255, 255), bg_color=(0, 0, 0), visibility_threshold=0.5):
+def transform(image, landmarks, color=(255, 255, 255), bg_color=(0, 0, 0), visibility_threshold=0.3):
     w, h = image.shape[1], image.shape[0]
 
-    # 背景
+    # set the background
     cv.rectangle(image, (0, 0), (w, h), bg_color, thickness=-1)
 
     landmark_infos = []
     for idx, landmark in enumerate(landmarks.landmark):
         landmark_x = min(int(landmark.x * w), w - 1)
         landmark_y = min(int(landmark.y * h), h - 1)
-        landmark_z = landmark.z
-        landmark_infos.append([idx, landmark.visibility, (landmark_x, landmark_y), landmark_z])
+        landmark_infos.append([idx, landmark.visibility, (landmark_x, landmark_y)])
 
-    (face_x, face_y), face_radius = min_enclosing_face_circle(landmark_infos)
-    face_x = int(face_x)
-    face_y = int(face_y)
-    face_radius = int(face_radius * 1.5)
-    # 头
-    cv.circle(image, (face_x, face_y), face_radius, color, -1)
-
-    stick_radius_01 = int(face_radius * (4 / 5))
-    stick_radius_02 = int(stick_radius_01 * (3 / 4))
-    stick_radius_03 = int(stick_radius_02 * (3 / 4))        
-
-    right_hip = landmark_infos[23]
-    left_hip = landmark_infos[24]
-    hip_x = int((right_hip[2][0] + left_hip[2][0]) / 2)
-    hip_y = int((right_hip[2][1] + left_hip[2][1]) / 2)
-    landmark_infos[23][2] = (hip_x, hip_y)
-    landmark_infos[24][2] = (hip_x, hip_y)
-
-    sorted_landmark_infos = sorted(landmark_infos, reverse=True, key=lambda x: x[3])
-
-    draw_list = [
-        11,  # 右腕
-        12,  # 左腕
-        23,  # 右脚
-        24,  # 左脚
-    ]
-
-    # 腕/脚
-    for landmark_info in sorted_landmark_infos:
-        idx = landmark_info[0]
-        if idx in draw_list:
-            info_01 = [x for x in landmark_infos if x[0] == idx][0]
-            info_02 = [x for x in landmark_infos if x[0] == (idx + 2)][0]
-            info_03 = [x for x in landmark_infos if x[0] == (idx + 4)][0]
-
-            if info_01[1] > visibility_threshold and info_02[1] > visibility_threshold:
-                image = draw_stick(image, info_01[2], stick_radius_01, info_02[2], stick_radius_02, color)
-            if info_02[1] > visibility_threshold and info_03[1] > visibility_threshold:
-                image = draw_stick(image, info_02[2], stick_radius_02, info_03[2], stick_radius_03, color)
-
-    return image
-
-
-def draw_stick(image, p1, p1_radius, p2, p2_radius, color):
-    cv.circle(image, p1, p1_radius, color, -1)
-    cv.circle(image, p2, p2_radius, color, -1)
-
-    draw_list = []
-    for idx in range(2):
-        rad = math.atan2(p2[1] - p1[1], p2[0] - p1[0])
-        rad = rad + (math.pi / 2) + (math.pi * idx)
-        p_x = int(p1_radius * math.cos(rad)) + p1[0]
-        p_y = int(p1_radius * math.sin(rad)) + p1[1]
-        draw_list.append([p_x, p_y])
-        p_x = int(p2_radius * math.cos(rad)) + p2[0]
-        p_y = int(p2_radius * math.sin(rad)) + p2[1]
-        draw_list.append([p_x, p_y])
-    points = np.array((draw_list[0], draw_list[1], draw_list[3], draw_list[2]))
-    cv.fillConvexPoly(image, points=points, color=color)
+    # set the face
+    (face_center_x, face_center_y), face_radius = min_enclosing_face_circle(landmark_infos)
+    face_center_x = int(face_center_x)
+    face_center_y = int(face_center_y)
+    face_radius = int(face_radius * 1.2)
+    cv.circle(image, (face_center_x, face_center_y), face_radius, color, -1)
+    # set the upper body
+    upper_body_center_x = (landmark_infos[11][2][0] + landmark_infos[12][2][0] + \
+        landmark_infos[24][2][0] + landmark_infos[23][2][0]) // 4
+    upper_body_center_y = (landmark_infos[11][2][1] + landmark_infos[12][2][1] + \
+        landmark_infos[24][2][1] + landmark_infos[23][2][1]) // 4
+    axes_h = int(((landmark_infos[11][2][0] + landmark_infos[23][2][0]) / 2 - \
+        (landmark_infos[12][2][0] + landmark_infos[24][2][0]) / 2) * 0.618)
+    axes_v = int(((landmark_infos[23][2][1] + landmark_infos[24][2][1]) / 2 - \
+        (landmark_infos[11][2][1] + landmark_infos[12][2][1]) / 2) * 0.618)
+    cv.ellipse(image, (upper_body_center_x, upper_body_center_y), (axes_h, axes_v), 0, 0, 360, color, -1)
+    # set left upper limb
+    point_set = np.array([
+        landmark_infos[11][2],
+        landmark_infos[13][2],
+        landmark_infos[15][2],
+        ((landmark_infos[17][2][0] + landmark_infos[19][2][0]) // 2, \
+            (landmark_infos[17][2][1] + landmark_infos[19][2][1]) // 2),
+    ])
+    for p1, p2 in zip(point_set, point_set[1:]):
+        cv.line(image, p1, p2, color, 2)
+    # set right upper limb
+    point_set = np.array([
+        landmark_infos[12][2],
+        landmark_infos[14][2],
+        landmark_infos[16][2],
+        ((landmark_infos[18][2][0] + landmark_infos[20][2][0]) // 2, \
+            (landmark_infos[18][2][1] + landmark_infos[20][2][1]) // 2),
+    ])
+    for p1, p2 in zip(point_set, point_set[1:]):
+        cv.line(image, p1, p2, color, 2)
+    # set left leg
+    point_set = np.array([
+        landmark_infos[23][2],
+        landmark_infos[25][2],
+        landmark_infos[27][2],
+        ((landmark_infos[29][2][0] + landmark_infos[31][2][0]) // 2, \
+            (landmark_infos[29][2][1] + landmark_infos[31][2][1]) // 2),
+    ])
+    for p1, p2 in zip(point_set, point_set[1:]):
+        cv.line(image, p1, p2, color, 2)
+    # set right leg
+    point_set = np.array([
+        landmark_infos[24][2],
+        landmark_infos[26][2],
+        landmark_infos[28][2],
+        ((landmark_infos[30][2][0] + landmark_infos[32][2][0]) // 2, \
+            (landmark_infos[30][2][1] + landmark_infos[32][2][1]) // 2),
+    ])
+    for p1, p2 in zip(point_set, point_set[1:]):
+        cv.line(image, p1, p2, color, 2)
 
     return image
+
+
+
+def min_enclosing_face_circle(landmark_infos):
+    point_set = np.empty((0, 2), int)
+
+    for idx in [2, 5, 7, 8, 9, 10]:
+        landmark_point = [
+            np.array((landmark_infos[idx][2][0], landmark_infos[idx][2][1]))
+        ]
+        point_set = np.append(point_set, landmark_point, axis=0)
+
+    return cv.minEnclosingCircle(points=point_set)
 
 
 if __name__ == "__main__":
@@ -101,15 +98,14 @@ if __name__ == "__main__":
     parser.add_argument("--video_device",             type=int,   default=0)
     parser.add_argument("--video_file",               type=str,   default="")
     parser.add_argument("--video_output_file",        type=str,   default="./output.mp4")
-    parser.add_argument("--video_width",              type=int,   default=0)
-    parser.add_argument("--video_height",             type=int,   default=0)
+    parser.add_argument("--video_output_width",       type=int,   default=0)
+    parser.add_argument("--video_output_height",      type=int,   default=0)
     parser.add_argument("--static_image_mode",        type=bool,  default=False)
     parser.add_argument("--model_complexity",         type=int,   default=1)
     parser.add_argument("--min_detection_confidence", type=float, default=0.5)
     parser.add_argument("--min_tracking_confidence",  type=float, default=0.5)
     args = parser.parse_args()
 
-    # init video capture handler
     cap = object()
     out = object()
     if args.video_file != "":
@@ -123,44 +119,48 @@ if __name__ == "__main__":
         w = cap.get(cv.CAP_PROP_FRAME_WIDTH)
         h = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
         fps = cap.get(cv.CAP_PROP_FPS)
-        if args.video_width > 0 and args.video_height > 0:
-            w = args.video_width
-            cap.set(cv.CAP_PROP_FRAME_WIDTH, w)
-            h = args.video_height
-            cap.set(cv.CAP_PROP_FRAME_HEIGHT, h)
-        print("Video <w: {}, h: {}, fps: {}>".format(w, h, fps))
-        out = cv.VideoWriter(args.video_output_file, apiPreference=0, 
-            fourcc=cv.VideoWriter_fourcc(*'MJPG'), fps=fps, frameSize=(int(w), int(h)))
+        print("Video <width: {}, height: {}, fps: {}>".format(w, h, fps))
+        if args.video_output_width > 0 and args.video_output_height > 0:
+            w = args.video_output_width
+            h = args.video_output_height
+        if args.video_file != "":
+            out = cv.VideoWriter(args.video_output_file, apiPreference=0, 
+                fourcc=cv.VideoWriter_fourcc(*'MJPG'), fps=fps, frameSize=(int(w), int(h)))
 
     mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose(
+    with mp_pose.Pose(
         static_image_mode=args.static_image_mode,
         model_complexity=args.model_complexity,
         min_detection_confidence=args.min_detection_confidence,
         min_tracking_confidence=args.min_tracking_confidence,
-    )
+    ) as pose:
 
-    image = np.zeros((int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), 3), np.uint8)
-    while 1:
-        ret, frame = cap.read()
-        if not ret:
-            print("Can't receive frame, exiting ...")
-            break
+        image = np.zeros((int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv.CAP_PROP_FRAME_WIDTH)), 3), np.uint8)
+        frame_idx = 0
+        while 1:
+            ret, frame = cap.read()
+            if not ret:
+                print("Cannot receive frame, exiting ...")
+                break
 
-        frame = cv.cvtColor(cv.flip(frame, 1), cv.COLOR_BGR2RGB)
-        frame.flags.writeable = False
-        results = pose.process(frame)
-        frame.flags.writeable = True
-        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+            st = time.time()
+            frame = cv.cvtColor(cv.flip(frame, 1), cv.COLOR_BGR2RGB)
+            frame.flags.writeable = False
+            results = pose.process(frame)
+            frame.flags.writeable = True
+            frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+            if results.pose_landmarks is not None:
+                image = transform(image, results.pose_landmarks)
+            ed = time.time()
+            print("Used {:.3f} secs to process frame-{:05}".format(ed - st, frame_idx))
 
-        if results.pose_landmarks is not None:
-            image = draw(image, results.pose_landmarks)
-        cv.imshow("Animation", image)
+            cv.imshow("Animation", image)
+            if args.video_file != "":
+                out.write(image)
+            if cv.waitKey(5) & 0xFF == 27:
+                break
 
-        out.write(image)
-        if cv.waitKey(5) & 0xFF == 27:
-            break
-
-    cap.release()
-    out.release()
-    cv.destroyAllWindows()
+        cap.release()
+        if args.video_file != "":
+            out.release()
+        cv.destroyAllWindows()
